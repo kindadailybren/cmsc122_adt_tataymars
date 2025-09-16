9bd0890728e4af3009ee50916e782cb7e8186c11
bren <breindelm6@gmail.com>
2025-09-16 18:57:05 +0800

    feat: gui for deck

---
+++ DeckImpl.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class DeckImpl {
+  public static void main(String[] args) {
+    String[] Cards = { "W", "F", "G", "S", "L", "C", "I", "B" };
+    CRDeck deck = new CRDeck(Cards);
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("===DeckImpl===");
+    System.out.println("1: Use");
+
+    int action, data, index;
+    while (true) {
+      System.out.print("Action Number:");
+      action = scanner.nextInt();
+      switch (action) {
+        case 1:
+          System.out.print("What Card:");
+          index = scanner.nextInt();
+          deck.useCard(index);
+          break;
+
+        default:
+          System.out.println("Command not found");
+          break;
+      }
+      deck.printCards();
+    }
+  }
+}
