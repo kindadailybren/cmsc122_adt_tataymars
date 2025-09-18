5dff40c9040047bc17b5ef9a4f929d0a64ae5539
bren <breindelm6@gmail.com>
2025-09-18 16:08:25 +0800

    chore: comments

--- CRDeck.java
+++ CRDeck.java
@@ -34,18 +34,19 @@ class CRDeck {
   void useCard(int choice) {
     Card currentCard = headCard;
     Card usedCard = null;
-    for (int i = 0; i < DECK_SIZE - 1; i++) {
-      if (choice == 0 && i == 0) {
+    for (int i = 0; i < DECK_SIZE - 1; i++) { // Goes through the list
+      if (choice == 0 && i == 0) { // If choice is first card, and iteration is at first card as well
         usedCard = currentCard;
         headCard = headCard.nextCard;
-      } else if (i == choice - 1) {
-        usedCard = currentCard.nextCard;
-        currentCard.nextCard = currentCard.nextCard.nextCard;
+      } else if (i == choice - 1) { // If you are at the previous card of the choice you made
+        usedCard = currentCard.nextCard; // Set the usedCard as the next
+        currentCard.nextCard = currentCard.nextCard.nextCard; // connects it to the next next, essentially removing the
+                                                              // used card from the hand
         continue;
       }
       currentCard = currentCard.nextCard;
     }
-    currentCard.nextCard = usedCard;
+    currentCard.nextCard = usedCard; // Now at the end node, push the new card there
     usedCard.nextCard = null;
   }
 
